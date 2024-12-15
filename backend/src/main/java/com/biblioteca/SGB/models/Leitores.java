ceb0e27f018c2fdd13be6228a2cb6c56521fe780
Camile <camileisi@alu.ufc.br>
2024-12-14 21:52:34 -0300

    implentação da funcionalidade cadastro de aluno

---
+++ backend/src/main/java/com/biblioteca/SGB/models/Leitores.java
@@ -0,0 +1,77 @@
+package com.biblioteca.SGB.models;
+
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class Leitores {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "Telefone")
+    private String telefone;
+
+
+    @Column(name = "Rua")
+    private String rua;
+
+    @Column(name= "Bairro")
+    private String bairro;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+}
