3ab649f61f8dab42473bee47238b102beb0f0f0b
Debora Viana <deborasviana3@gmail.com>
2025-01-04 21:00:28 -0300

    adicionada interface Aluno

---
+++ frontend/my-app/src/types/alunos.d.ts
@@ -0,0 +1,12 @@
+export interface Aluno {    
+    bairro: string;        
+    nome: string;              
+    rua: string;       
+    telefone: string;       
+    anoLetivo: string;     
+    matricula: string;         
+    serie: string;             
+    turma: string;
+    numero: string;            
+  }
+  
