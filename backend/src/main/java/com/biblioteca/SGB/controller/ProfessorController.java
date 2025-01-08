b34d4ac74b872b24d6bc8661c975563cc48c116e
Werley <franciscowerley08@gmail.com>
2025-01-08 15:28:12 -0300

    Implementação do listar(Visualizar) professores

--- backend/src/main/java/com/biblioteca/SGB/controller/ProfessorController.java
+++ backend/src/main/java/com/biblioteca/SGB/controller/ProfessorController.java
@@ -1,9 +1,12 @@
 package com.biblioteca.SGB.controller;
 
+import com.biblioteca.SGB.models.Aluno;
 import com.biblioteca.SGB.models.Professor;
 import com.biblioteca.SGB.services.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/professores")
@@ -16,6 +19,13 @@ public class ProfessorController {
     public Professor cadastrarProfessor(@RequestBody Professor professor) {
 
         return professorService.cadastrarProfessor(professor);
+
+    }
+
+    @GetMapping
+    public List<Professor> listarProfessores(){
+
+        return professorService.listarProfessores();
 
     }
 
