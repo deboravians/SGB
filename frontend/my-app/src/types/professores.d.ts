a9de0374d7ccc965222342911d0b5daa1969dc2f
Debora Viana <deborasviana3@gmail.com>
2025-01-12 02:22:27 -0300

    criada interface para professor

---
+++ frontend/my-app/src/types/professores.d.ts
@@ -0,0 +1,8 @@
+export interface Professor {    
+    bairro: string;        
+    nome: string;              
+    rua: string;       
+    telefone: string;       
+    disciplina: string;            
+  }
+  
