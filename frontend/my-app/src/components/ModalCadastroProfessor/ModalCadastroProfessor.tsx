ec92675478806c93db4553ee2045b239cc24af85
Debora Viana <deborasviana3@gmail.com>
2025-01-12 02:25:04 -0300

    adicionados modal e css de cadastro de professores

---
+++ frontend/my-app/src/components/ModalCadastroProfessor/ModalCadastroProfessor.tsx
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+import styles from "./ModalCadastroProfessor.module.css";
+import { Professor } from "../../types/professores";
+import { cadastrarProfessor } from "../../api/professores";
+
+interface ModalCadastroProfessorProps {
+  fecharModal: () => void;
+  salvarProfessor: (professor: Professor) => void;
+}
+
+const ModalCadastroProfessor: React.FC<ModalCadastroProfessorProps> = ({
+  fecharModal,
+  salvarProfessor,
+}) => {
+  const [formData, setFormData] = useState<Professor>({
+    bairro: "",
+    nome: "",
+    rua: "",
+    telefone: "",
+    disciplina: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setErrorMessage(null);
+  
+    try {
+      const savedProfessor = await cadastrarProfessor(formData);
+      salvarProfessor(savedProfessor);
+      fecharModal();
+    } catch (error: unknown) {
+      const errorMessage = (error as Error).message || "Erro desconhecido.";
+      setErrorMessage(errorMessage);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  return (
+
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContent}>
+        <h2 className={styles.modalTitle}>Cadastrar Professor</h2>
+        {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+        <form onSubmit={handleSubmit}>
+          <h3 className={styles.sectionTitle}>Informações Gerais</h3>
+          <div className={styles.generalInfo}>
+            <div className={styles.formGroup}>
+              <label htmlFor="nome">Nome:</label>
+              <input
+                value={formData.nome}
+                onChange={handleChange}
+                type="text"
+                id="nome"
+                placeholder="Digite o nome do professor"
+                className={styles.inputField1}
+              />
+            </div>
+            <div className={styles.row}>
+              <div className={styles.formGroup}>
+                <label htmlFor="telefone">Telefone:</label>
+                <input
+                  value={formData.telefone}
+                  onChange={handleChange}
+                  type="text"
+                  id="telefone"
+                  placeholder="(00) 00000-0000"
+                  className={styles.inputField2}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="disciplina">Disciplina:</label>
+                <input
+                  value={formData.disciplina}
+                  onChange={handleChange}
+                  type="text"
+                  id="disciplina"
+                  placeholder="Português"
+                  className={styles.inputField3}
+                />
+              </div>
+            </div>
+          </div>
+
+          <h3 className={styles.sectionTitle}>Endereço</h3>
+          <div className={styles.addressInfo}>
+            <div className={styles.row}>
+              <div className={styles.formGroup}>
+                <label htmlFor="rua">Rua:</label>
+                <input
+                  value={formData.rua}
+                  onChange={handleChange}
+                  type="text"
+                  id="rua"
+                  placeholder="Digite a rua"
+                  className={styles.inputField1}
+                />
+              </div>
+              <div className={styles.formGroup}>
+                <label htmlFor="numero">Número:</label>
+                <input
+                  type="text"
+                  id="numero"
+                  placeholder="0000"
+                  className={styles.inputField3}
+                />
+              </div>
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="bairro">Bairro:</label>
+              <input
+                value={formData.bairro}
+                onChange={handleChange}
+                type="text"
+                id="bairro"
+                placeholder="Digite o bairro"
+                className={styles.inputField2}
+              />
+            </div>
+          </div>
+
+          <div className={styles.modalActions}>
+            <button type="submit" className={styles.botaoCadastrar} disabled={isSubmitting}>
+              {isSubmitting ? "Salvando..." : "Salvar"}
+            </button>
+            <button
+              type="button"
+              className={styles.botaoCancelar}
+              onClick={fecharModal} // Fecha a modal
+              disabled={isSubmitting}
+            >
+              Cancelar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ModalCadastroProfessor;
