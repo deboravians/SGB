7c6f7b9d1ae8c54318873985b9971083cf73f910
Debora Viana <deborasviana3@gmail.com>
2025-01-19 12:33:32 -0300

    home atualizada

--- frontend/my-app/src/pages/Home/Home.tsx
+++ frontend/my-app/src/pages/Home/Home.tsx
@@ -1,14 +1,61 @@
+import CardInfors from "../../components/CardInfors/CardInfors";
 import styles from "./home.module.css";
+import { Aluno } from "../../types/alunos";
+import { listarAlunos } from "../../api/alunos";
+import { useState, useEffect } from "react";
+import { Professor } from "../../types/professores";
+import { listarProfessores } from "../../api/professores";
 
 const Home = () => {
+
+  const [alunos, setAlunos] = useState<Aluno[]>([]);
+  const [professores, setProfessores] = useState<Professor[]>([]);
+
+  const carregarDados = async () => {
+    try {
+      const [alunosData, professoresData] = await Promise.all([
+        listarAlunos(), // Requisição dos alunos
+        listarProfessores(), // Requisição dos professores
+      ]);
+      setAlunos(alunosData); // Atualiza alunos
+      setProfessores(professoresData); // Atualiza professores
+    } catch (error) {
+      alert("Erro ao carregar dados.");
+    }
+  };
+
+  useEffect(() => {
+    carregarDados(); // Carrega os dados ao montar o componente
+  }, []);
+
   return (
-    <div className={styles.telaHome}>
-      <img
-        src="./public/assets/imagemMain.svg"
-        alt="Imagem Main"
-        className={styles.imagemMain}
-      />
-    </div>
+    <>
+      <div className={styles.mainContent}>
+        <div className={styles.home}>
+          <div className={styles.header}>
+            <img className={styles.icone} src="/public/assets/iconEscola.svg" alt="Ícone Escola" />
+            <h1 className={styles.titulo}>Biblioteca Rachel de Queiroz - EEMTI Coronel Virgílio Távora</h1>
+          </div>
+          <div className={styles.divisao}></div>
+          <div className={styles.content}>
+            <div className={styles.cards}>
+              <h1 className={styles.titulo2}>Seja bem-vindo! Dados gerais da biblioteca:</h1>
+              <CardInfors quantidade={0} descricao="Livros Cadastrados" />
+              <CardInfors quantidade={alunos.length + professores.length} descricao="Leitores Cadastrados" />
+              <CardInfors quantidade={0} descricao="Empréstimos Ativos" />
+              <CardInfors quantidade={0} descricao="Empréstimos em Atraso" />
+            </div>
+            <div className={styles.imagemContainer}>
+              <img
+                src="/public/assets/imagemEstudante.svg"
+                alt="Cadastrar"
+                className={styles.imagem}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
