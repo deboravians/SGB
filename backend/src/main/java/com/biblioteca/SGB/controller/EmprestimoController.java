854dfc3a89c370a89befbc405624494d87d9f283
Werley <franciscowerley08@gmail.com>
2025-01-27 16:00:22 -0300

    Implementação do Back de Listar Emprestimos de alunos e professores

--- backend/src/main/java/com/biblioteca/SGB/controller/EmprestimoController.java
+++ backend/src/main/java/com/biblioteca/SGB/controller/EmprestimoController.java
@@ -1,8 +1,12 @@
 package com.biblioteca.SGB.controller;
 
+import com.biblioteca.SGB.models.Classificacao;
+import com.biblioteca.SGB.models.Emprestimo;
 import com.biblioteca.SGB.services.EmprestimoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/emprestimos")
@@ -10,5 +14,12 @@ public class EmprestimoController { // Usar essa classe pra fazer o requisito de
 
     @Autowired
     private EmprestimoService emprestimoService;
+
+    @GetMapping
+    public List<Emprestimo> listarEmprestimos(){
+
+        return emprestimoService.listarEmprestimos();
+
+    }
 
 }
--- backend/src/main/java/com/biblioteca/SGB/services/EmprestimoService.java
+++ backend/src/main/java/com/biblioteca/SGB/services/EmprestimoService.java
@@ -1,9 +1,19 @@
 package com.biblioteca.SGB.services;
 
+import com.biblioteca.SGB.models.Aluno;
+import com.biblioteca.SGB.models.Emprestimo;
+import com.biblioteca.SGB.repository.EmprestimoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmprestimoService {
 
+    @Autowired
+    private EmprestimoRepository emprestimoRepository;
+
+    public List<Emprestimo> listarEmprestimos(){ return emprestimoRepository.findAll(); }
 
 }
