bc59b7b51eec1d7109aeb18785a16cc324583621
Werley <franciscowerley08@gmail.com>
2025-01-28 19:20:46 -0300

    Criação do DTO de Usuario e Refatoração do Controller de Usuario, para introdução da classe DTO

--- backend/src/main/java/com/biblioteca/SGB/controller/UsuarioController.java
+++ backend/src/main/java/com/biblioteca/SGB/controller/UsuarioController.java
@@ -1,27 +1,26 @@
 package com.biblioteca.SGB.controller;
 
+import com.biblioteca.SGB.dto.UsuarioDTO;
 import com.biblioteca.SGB.models.Usuario;
-
 import com.biblioteca.SGB.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuarios")
-public class UsuarioController{
+public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
     @PostMapping
-    public String logar(@RequestBody Usuario usuario){
+    public String logar(@RequestBody UsuarioDTO usuarioDTO) {
+
+        Usuario usuario = new Usuario(
+                usuarioDTO.getLogin(),
+                usuarioDTO.getSenha()
+        );
 
         return usuarioService.logar(usuario);
-
-
     }
-
 }
