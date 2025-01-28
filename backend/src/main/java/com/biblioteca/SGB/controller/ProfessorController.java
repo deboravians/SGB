634227580f20c4b37c3eb57de07e03a18bf68970
Werley <franciscowerley08@gmail.com>
2025-01-28 04:45:06 -0300

    Refatoração do controller de Professor para introdução da classe DTO

--- backend/src/main/java/com/biblioteca/SGB/controller/ProfessorController.java
+++ backend/src/main/java/com/biblioteca/SGB/controller/ProfessorController.java
@@ -1,12 +1,13 @@
 package com.biblioteca.SGB.controller;
 
-import com.biblioteca.SGB.models.Aluno;
+import com.biblioteca.SGB.dto.ProfessorDTO;
 import com.biblioteca.SGB.models.Professor;
 import com.biblioteca.SGB.services.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/professores")
@@ -16,24 +17,33 @@ public class ProfessorController {
     private ProfessorService professorService;
 
     @PostMapping()
-    public Professor cadastrarProfessor(@RequestBody Professor professor) {
+    public ProfessorDTO cadastrarProfessor(@RequestBody ProfessorDTO professorDTO) {
+        Professor professor = new Professor(
+                professorDTO.getNome(),
+                professorDTO.getTelefone(),
+                professorDTO.getRua(),
+                professorDTO.getBairro(),
+                professorDTO.getCpf(),
+                professorDTO.getDisciplina()
+        );
 
-        return professorService.cadastrarProfessor(professor);
-
+        Professor professorCadastrado = professorService.cadastrarProfessor(professor);
+        return ProfessorDTO.fromProfessor(professorCadastrado);
     }
 
     @GetMapping
-    public List<Professor> listarProfessores(){
+    public List<ProfessorDTO> listarProfessores() {
+        List<Professor> professores = professorService.listarProfessores();
 
-        return professorService.listarProfessores();
-
+        return professores.stream()
+                .map(ProfessorDTO::fromProfessor)
+                .collect(Collectors.toList());
     }
 
     @DeleteMapping("/{cpf}")
-    public void excluirProfessor(@PathVariable String cpf){
+    public void excluirProfessor(@PathVariable String cpf) {
         professorService.excluirProfessor(cpf);
     }
-
 }
 
 
--- backend/src/main/java/com/biblioteca/SGB/models/Professor.java
+++ backend/src/main/java/com/biblioteca/SGB/models/Professor.java
@@ -16,6 +16,15 @@ public class Professor extends Leitores{
     @Column(nullable = false)
     private String disciplina;
 
+    public Professor(){
+    }
+
+    public Professor(String nome, String telefone, String rua, String bairro, String cpf, String disciplina) {
+        super(nome, telefone, rua, bairro);
+        this.cpf = cpf;
+        this.disciplina = disciplina;
+    }
+
     public String getDisciplina() {
         return disciplina;
     }
