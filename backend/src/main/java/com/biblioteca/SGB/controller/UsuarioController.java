3e7a6f0e4371e5719616a1b2bdd83afde5e67d45
Werley <franciscowerley08@gmail.com>
2025-02-12 20:13:38 -0300

    Inversão de dependencia entre o controller e o service de Usuario

--- backend/src/main/java/com/biblioteca/SGB/controller/UsuarioController.java
+++ backend/src/main/java/com/biblioteca/SGB/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.biblioteca.SGB.dto.UsuarioDTO;
 import com.biblioteca.SGB.mapper.UsuarioMapper;
 import com.biblioteca.SGB.models.Usuario;
 import com.biblioteca.SGB.services.UsuarioService;
+import com.biblioteca.SGB.services.interfaces.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     @Autowired
-    private UsuarioService usuarioService;
+    private IUsuarioService usuarioService;
 
     @PostMapping
     public String logar(@RequestBody UsuarioDTO usuarioDTO) {
