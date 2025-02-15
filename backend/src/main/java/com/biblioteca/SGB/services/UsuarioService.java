26385fb023ab23b1d171a73fcbf129a207f8e350
Camile <camileisi@alu.ufc.br>
2025-02-15 13:40:50 -0300

    Adicionada a inversão de dependência no usuario service e repository.

--- backend/src/main/java/com/biblioteca/SGB/services/UsuarioService.java
+++ backend/src/main/java/com/biblioteca/SGB/services/UsuarioService.java
@@ -1,7 +1,7 @@
 package com.biblioteca.SGB.services;
 
 import com.biblioteca.SGB.models.Usuario;
-import com.biblioteca.SGB.repository.UsuarioRepository;
+import com.biblioteca.SGB.repository.interfaces.UsuarioRepository;
 import com.biblioteca.SGB.services.interfaces.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,12 +9,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class UsuarioService implements IUsuarioService {
 
+    private final UsuarioRepository usuarioRepository;
+
     @Autowired
-    private UsuarioRepository UsuarioRepository;
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
 
     public String logar(Usuario usuario){
 
-        Usuario usuarioLogado = UsuarioRepository.findByLogin(usuario.getLogin());
+        Usuario usuarioLogado = usuarioRepository.findByLogin(usuario.getLogin());
 
         if(usuarioLogado == null){ return "0"; }
 
