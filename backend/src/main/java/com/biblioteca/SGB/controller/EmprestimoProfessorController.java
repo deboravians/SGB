92a246f0c683a98a68b1f24ca36a8d0a2d736b40
Werley <franciscowerley08@gmail.com>
2025-02-16 02:03:27 -0300

    Criação do construtor de EmprestimoProfessorController

--- backend/src/main/java/com/biblioteca/SGB/controller/EmprestimoProfessorController.java
+++ backend/src/main/java/com/biblioteca/SGB/controller/EmprestimoProfessorController.java
@@ -3,6 +3,7 @@ package com.biblioteca.SGB.controller;
 import com.biblioteca.SGB.dto.EmprestimoDTO;
 import com.biblioteca.SGB.mapper.EmprestimoMapper;
 import com.biblioteca.SGB.models.Emprestimo;
+import com.biblioteca.SGB.services.interfaces.IEmprestimoAlunoService;
 import com.biblioteca.SGB.services.interfaces.IEmprestimoProfessorService;
 import com.biblioteca.SGB.services.interfaces.IEmprestimoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,15 @@ import java.util.stream.Collectors;
 @RequestMapping("/emprestimos/professores")
 public class EmprestimoProfessorController {
 
-    @Autowired
-    private IEmprestimoProfessorService emprestimoProfessorService;
+    private final IEmprestimoProfessorService emprestimoProfessorService;
+    private final IEmprestimoService emprestimoService;
 
-    @Autowired
-    private IEmprestimoService emprestimoService;
+    public EmprestimoProfessorController(IEmprestimoProfessorService emprestimoProfessorService,
+                                         IEmprestimoService emprestimoService) {
+
+        this.emprestimoProfessorService = emprestimoProfessorService;
+        this.emprestimoService = emprestimoService;
+    }
 
     @PostMapping
     public EmprestimoDTO cadastrarEmprestimo(@RequestBody EmprestimoDTO emprestimoDTO,
