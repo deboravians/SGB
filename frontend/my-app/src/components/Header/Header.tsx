41ea383856280df0cc1b9294d8105fcb388c2dbf
Anaildo silva <anaildonascimentosilva074@gmail.com>
2025-02-25 07:42:42 -0300

    Aplicando mudanças na header

--- frontend/my-app/src/components/Header/Header.tsx
+++ frontend/my-app/src/components/Header/Header.tsx
@@ -1,18 +1,15 @@
-import { Link } from "react-router-dom";
 import style from './header.module.css';
 
 const Header = () => {
     return (
         <header className={style.divHeader}>
-            <Link to="/home">
-                <div className={style.divlogo}>
-                    <img
-                        src="assets/logoMinhaBibliotecaBranca.svg"
-                        alt="Ícone minhabiblioteca"
-                        className={style.iconLogo}
-                    />
-                </div>
-            </Link>
+            <div className={style.divlogo}>
+                <img
+                    src="assets/logoMinhaBibliotecaBranca.svg"
+                    alt="Ícone minhabiblioteca"
+                    className={style.iconLogo}
+                />
+            </div>
         </header>
     );
 }
